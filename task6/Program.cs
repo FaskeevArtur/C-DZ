41dc1fd915b85bc43b82c7bb56feb07cda058dcc
Artur <xduhbadun@yandex.ru>
2022-09-18 11:32:01 +0500

    добавили третье задание

---
+++ task6/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("введите первое число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+if (userNumber1 % 2 == 0)
+{
+    Console.WriteLine($"{userNumber1} четное число");
+}
+else
+{
+    Console.WriteLine($"{userNumber1} нечетное число");
+}
